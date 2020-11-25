fd99fa8cfa12c8955e9a834a00edc833663e2d66
kshitij7890 <kshitijsingla7890@gmail.com>
2020-11-25 23:56:57 +0530

    trees commit

--- 6) Binary Trees/3) height_bt.cpp
+++
@@ -1,7 +0,0 @@
- int height(struct Node* node){
-        if(node==NULL) return NULL;
-        else 
-        {
-           return 1 + max(height(node->left),height(node->right));
-        }  
-    }
