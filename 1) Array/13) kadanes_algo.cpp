6b88f7525b683681d6793c3e6570181be8b0c94c
kshitij7890 <kshitijsingla7890@gmail.com>
2020-12-03 23:45:58 +0530

    arrays commit

--- 1) Array/13) kadanes_algo.cpp
+++ 1) Array/13) kadanes_algo.cpp
@@ -1,38 +1,29 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-// Function to find subarray with maximum sum
 int maxSubarraySum(int arr[], int n)
 {
-
-    int res = arr[0];
-    int currmax = arr[0];
-    for (int i = 1; i < n; i++)
-    {
-        currmax = max(currmax + arr[i], arr[i]);
-        res = max(res, currmax);
+    int res=arr[0];
+    int currmax=arr[0];
+    for(int i=1;i<n;i++)
+    {   
+        currmax = max(currmax+arr[i],arr[i]);
+        res=max(res,currmax);
     }
     return res;
 }
 
-// { Driver Code Starts.
-
 int main()
 {
-    int t, n;
-
-    cin >> t;
-    while (t--)
+    int t,n;
+    
+    cin>>t; 
+    while(t--) 
     {
-
-        cin >> n;
-
+        cin>>n; 
         int a[n];
-
-        for (int i = 0; i < n; i++)
-            cin >> a[i];
-
+        for(int i=0;i<n;i++)
+            cin>>a[i]; 
         cout << maxSubarraySum(a, n) << endl;
     }
 }
-// } Driver Code Ends
