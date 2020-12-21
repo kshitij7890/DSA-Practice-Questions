0daa8cee5264e2eeb63eeae49b2242336f38f0aa
kshitij7890 <kshitijsingla7890@gmail.com>
2020-12-21 21:55:48 +0530

    LL commit

---
+++ 5) Linked List/3) Detect Loop.cpp
@@ -0,0 +1,27 @@
+/*
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+
+*/
+
+bool detectLoop(Node* head)
+{
+   Node *slow_p = head, *fast_p = head; 
+  
+    while (fast_p!=NULL && fast_p->next!=NULL) { 
+        slow_p = slow_p->next; 
+        fast_p = fast_p->next->next; 
+        if (slow_p == fast_p) { 
+            return true; 
+        } 
+    } 
+    return false; 
+    
+}
