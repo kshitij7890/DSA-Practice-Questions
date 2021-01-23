d3e383f4cff75f2b71e0a99d05c31edaec948c18
kshitij7890 <kshitijsingla7890@gmail.com>
2021-01-23 22:35:16 +0530

    searching sorting commit

--- 4) Searching and Sorting/8) Majority Element.cpp
+++ 4) Searching and Sorting/8) Majority Element.cpp
@@ -1,6 +1,18 @@
 //TC : O(n)
 //AS : O(1)
 
+/*
+
+Input : {3, 3, 4, 2, 4, 4, 2, 4, 4}
+Output : 4
+Explanation: The frequency of 4 is 5 which is greater
+than the half of the size of the array size. 
+
+Input : {3, 3, 4, 2, 4, 4, 2, 4}
+Output : No Majority Element
+
+*/
+
 int majorityElement(int arr[], int n)
 {
     int idx = 0, count = 1;
