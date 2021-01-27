52eb853b71978f0bfef62ed4533c41b300a3e99b
kshitij7890 <kshitijsingla7890@gmail.com>
2021-01-27 10:13:52 +0530

    DP commit

--- 14) DP/Ways to write n as sum.cpp
+++ 14) DP/Ways to write n as sum.cpp
@@ -1,1 +1,43 @@
 // Ways to write n as sum
+// Same as Coin Change Problem
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int countWays(int);
+
+int main()
+{
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        cout<<countWays(n)<<endl;
+    }
+    return 0;
+}
+
+int countWays(int n)
+{
+    vector<long long int>arr;
+    for(int i=1;i<n;i++)
+    arr.push_back(i);
+    
+    int mod = (int)(1e9+7);
+    
+    long long int dp[n+1]={0};
+    dp[0]=1;
+    
+    for(auto x:arr)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            if(i>=x)
+                dp[i]=((dp[i]%mod)+(dp[i-x]%mod))%mod;
+        }
+    }
+    return dp[n]%mod;
+}
