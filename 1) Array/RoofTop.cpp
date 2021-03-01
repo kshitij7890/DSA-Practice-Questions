f6712db3f5bda81acf3c79db1021f57ea5f6f13a
kshitij7890 <kshitijsingla7890@gmail.com>
2021-03-01 23:16:55 +0530

    arrays commit

--- 1) Array/RoofTop.cpp
+++ 1) Array/RoofTop.cpp
@@ -1,20 +1,22 @@
-int maxStep(int arr[], int len){
-        
-        int count = 0; 
-    int maximum = 0; 
-  
-    for (int index = 1; index < len; index++) { 
-          
-        // count the number of consecutive  
-        // increasing height building 
-        if (arr[index] > arr[index - 1]) 
-            count++; 
+int maxStep(int arr[], int len)
+{
+
+    int count = 0;
+    int maximum = 0;
+
+    for (int index = 1; index < len; index++)
+    {
+
+        // count the number of consecutive
+        // increasing height building
+        if (arr[index] > arr[index - 1])
+            count++;
         else
-        { 
-            maximum = max(maximum, count); 
-            count = 0; 
-        } 
-    } 
-      
-    return max(maximum, count);   
+        {
+            maximum = max(maximum, count);
+            count = 0;
+        }
     }
+
+    return max(maximum, count);
+}
